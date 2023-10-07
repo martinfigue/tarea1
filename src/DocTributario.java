49ad4cc1ac13ca95c9ad919ef9a7281ea5e7a637
idiazbarrientos <idiazbarrientos20@gmail.com>
2023-10-07 18:29:43 -0300

    Update DocTributario.java
    terminar doctributario

--- src/DocTributario.java
+++ src/DocTributario.java
@@ -1,13 +1,49 @@
-import java.util.ArrayList;
+
 import java.util.Date;
 abstract class DocTributario{
     private OrdenCompra ordencompra;
     private String numero;
     private String rut;
     private Date fecha;
-    public DocTributario(String n, String r, Date f){
+    private Direccion direccion;
+    public DocTributario(String n, String r, Date f, Direccion direccion, OrdenCompra ordenCompra){
         numero = n;
         rut = r;
         fecha = f;
+    }
+    public String getNumero(){
+        return numero;
+    }
+    public void setNumero(String n){
+        numero = n;
+    }
+    public String getRut(){
+        return rut;
+    }
+    public void setRut(String r){
+        rut = r;
+    }
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date f) {
+        fecha = f;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+    public OrdenCompra getCompra() {
+        return ordencompra;
+    }
+
+    public void setCompra(OrdenCompra ordencompra) {
+        this.ordencompra = ordencompra;
     }
 }
