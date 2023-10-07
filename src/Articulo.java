15a1569ccf5aa09c741b551f9c7d43dff7cb2c68
idiazbarrientos <idiazbarrientos20@gmail.com>
2023-10-07 19:32:16 -0300

    Update Articulo.java
    articulo getter y setters y toString

--- src/Articulo.java
+++ src/Articulo.java
@@ -17,7 +17,29 @@ public class Articulo {
     public float getPeso(){
         return peso;
     }
+    public void setPeso(float kg){
+        peso = kg;
+    }
     public float getPrecio(){
         return precio;
+    }
+    public void setPrecio(float price){
+        precio = price;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nom){
+        nombre = nom;
+    }
+    public String getDescripcion(){
+        return descripcion;
+    }
+    public void setDescripcion(String desc){
+        descripcion = desc;
+    }
+    public String toString(){
+        return "Articulo" + nombre + descripcion 
+                          + peso + precio;  
     }
 }
