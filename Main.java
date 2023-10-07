2521153fdc9b3c8e3f49e8461d5805eab65b3973
idiazbarrientos <idiazbarrientos20@gmail.com>
2023-10-07 15:29:02 -0300

    Update Main.java
    doctributario

--- Main.java
+++ Main.java
@@ -24,6 +24,8 @@ class DetalleOrden{
     public DetalleOrden(int n, OrdenCompra o) {
         cantidad = n;
         orden = o;
+        o.print();
+
     }
 }
 class Articulo{
@@ -49,6 +51,7 @@ class Articulo{
 }
 class Direccion{
     private String direccion;
+    private ArrayList<DocTributario> doctributario;
 
     public Direccion() {
     }
@@ -58,58 +61,50 @@ abstract class DocTributario{
     private String numero;
     private String rut;
     private Date fecha;
-    public DocTributario(){
+    private Direccion direccion;
+    public DocTributario(String n, String r, Date f){
+        numero = n;
+        rut = r;
+        fecha = f;
     }
 }
 class  Boleta extends DocTributario{
-    public Boleta(String n, String r, Date f){
-        numero = n;
-        rut = r;
-        fecha = f;
+    public Boleta(String n, String r, Date f,){
+        super(n,r,t);
     }
-
 }
 class Factura extends DocTributario{
-    public Boleta(String n, String r, Date f){
-        numero = n;
-        rut = r;
-        fecha = f;
+    public Factura(String n, String r, Date f) {
+        super(n, r, f);
+        
     }
-
 }
 abstract class Pago{
     private float monto;
     private Date fecha;
-    private OrdenCompra ordencompra;
-    public Pago(float m, Date f, OrdenCompra orden){
+    public Pago(float m, Date f){
         monto = m;
         fecha = f;
-        ordencompra = orden;
     }
     public float getMonto(){
         return monto;
     }
-    public OrdenCompra getOrden(){
-        return ordencompra;
-    }
 }
 class Efectivo extends Pago{
-    public Efectivo (float m, Date f, OrdenCompra o){
-        super(m,f,o);
+    public Efectivo (float m, Date f){
+        super(m,f);
     }
 
-    public float calcDevolucion(){
-        float monto = getMonto();
-        OrdenCompra orden = getOrden();
-        float precio = orden.calcPrecio();
-        return monto - precio;
+    public float calcDevolucion(float precio){
+        float m = getMonto();
+        return m - precio;
     }
 }
 class Transferencia extends Pago{
     private String banco;
     private String numCuenta;
-    public Transferencia(String bank, String ncuenta, float m, Date f, OrdenCompra o) {
-        super(m,f,o);
+    public Transferencia(String bank, String ncuenta, float m, Date f) {
+        super(m,f);
         banco = bank;
         numCuenta = ncuenta;
     }
@@ -117,8 +112,8 @@ class Transferencia extends Pago{
 class Tarjeta extends Pago{
     private String tipo;
     private String numTransaccion;
-    public Tarjeta(String type, String ntransaccion, float m, Date f, OrdenCompra o) {
-        super(m,f,o);
+    public Tarjeta(String type, String ntransaccion, float m, Date f) {
+        super(m,f);
         tipo = type;
         numTransaccion = ntransaccion;
     }
