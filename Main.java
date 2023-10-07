49f340f221f5f90c58f1d275dc98af6b2cc576f3
martinfigue <martin.figueroahdez@gmail.com>
2023-10-07 15:13:09 -0300

    metodos pago

--- Main.java
+++ Main.java
@@ -24,8 +24,6 @@ class DetalleOrden{
     public DetalleOrden(int n, OrdenCompra o) {
         cantidad = n;
         orden = o;
-        o.print();
-
     }
 }
 class Articulo{
@@ -72,29 +70,36 @@ class Factura extends DocTributario{
 abstract class Pago{
     private float monto;
     private Date fecha;
-    public Pago(float m, Date f){
+    private OrdenCompra ordencompra;
+    public Pago(float m, Date f, OrdenCompra orden){
         monto = m;
         fecha = f;
+        ordencompra = orden;
     }
     public float getMonto(){
         return monto;
     }
+    public OrdenCompra getOrden(){
+        return ordencompra;
+    }
 }
 class Efectivo extends Pago{
-    public Efectivo (float m, Date f){
-        super(m,f);
+    public Efectivo (float m, Date f, OrdenCompra o){
+        super(m,f,o);
     }
 
-    public float calcDevolucion(float precio){
-        float m = getMonto();
-        return m - precio;
+    public float calcDevolucion(){
+        float monto = getMonto();
+        OrdenCompra orden = getOrden();
+        float precio = orden.calcPrecio();
+        return monto - precio;
     }
 }
 class Transferencia extends Pago{
     private String banco;
     private String numCuenta;
-    public Transferencia(String bank, String ncuenta, float m, Date f) {
-        super(m,f);
+    public Transferencia(String bank, String ncuenta, float m, Date f, OrdenCompra o) {
+        super(m,f,o);
         banco = bank;
         numCuenta = ncuenta;
     }
@@ -102,8 +107,8 @@ class Transferencia extends Pago{
 class Tarjeta extends Pago{
     private String tipo;
     private String numTransaccion;
-    public Tarjeta(String type, String ntransaccion, float m, Date f) {
-        super(m,f);
+    public Tarjeta(String type, String ntransaccion, float m, Date f, OrdenCompra o) {
+        super(m,f,o);
         tipo = type;
         numTransaccion = ntransaccion;
     }
