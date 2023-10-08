a6125e7fb10628b3c9cfa22df145f9f08351f31e
idiazbarrientos <idiazbarrientos20@gmail.com>
2023-10-07 22:26:52 -0300

    Update OrdenCompra.java
    setter y gettes orden compra

--- src/OrdenCompra.java
+++ src/OrdenCompra.java
@@ -7,19 +7,39 @@ public class OrdenCompra {
     private ArrayList<Pago> pagos;
     private Date fecha;
     private String estado;
-    public OrdenCompra(Cliente c, Date f, String state) {
+    private DocTributario doctributario;
+    public OrdenCompra(Cliente c, Date f, ArrayList<Articulo> A, int[] cantArticulos) {
         cliente = c;
         fecha = f;
-        estado = state;
         pagos = new ArrayList<>();
-        detalles = new ArrayList<>();
+        for(int i = 0; i<A.size(); i++){
+            DetalleOrden detalleOrden = new DetalleOrden(cantArticulos[i], this, A.get(i));
+            detalles.add(detalleOrden);
+        }
     }
+    public Date getFecha(){
+        return fecha;
+    }
+    public void setFecha(Date f){
+        fecha = f;
+    }
+    public String getEstado(){
+        return estado;
+    } 
+    public void setEstado(String estado){
+        this.estado = estado;
+    }
+    public Cliente getCliente(){
+        return cliente;
+    }
+    public void setCliente(Cliente c){
+        cliente = c;
+    }
+    public 
     public void addPago(Pago p){
         pagos.add(p);
     }
-    public void addDetalle(DetalleOrden d, int cant){
-        detalles.add(new DetalleOrden(cant, this));
-    }
+    
     public float calcPrecioSinIVA(){
         float precioSinIva = 0;
         for(int i = 0; i<detalles.size(); i++){
@@ -48,4 +68,5 @@ public class OrdenCompra {
         }
         return peso;
     }
+
 }
