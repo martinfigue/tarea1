a5a2c858843cc145243030ed8ca8d6d99f3a0376
idiazbarrientos <idiazbarrientos20@gmail.com>
2023-10-07 22:15:22 -0300

    Update Transferencia.java
    trnasferencia sett y getter y tostring

--- src/Transferencia.java
+++ src/Transferencia.java
@@ -8,7 +8,20 @@ class Transferencia extends Pago{
         banco = bank;
         numCuenta = ncuenta;
     }
+    public String getBanco(){
+        return banco;
+    }
+    public void setBanco(String bank){
+        banco = bank;
+    }
+    public String getNumcuenta(){
+        return numCuenta;
+    }
+    public void setNumcuenta(String ncuenta){
+        numCuenta = ncuenta;
+    }
     public String toString() {
-        return "Efectivo " + super.toString();
+        return "Transferencia" + "Banco:"+ banco 
+        + "Cuenta"+ numCuenta+ super.toString();
     }
 }
